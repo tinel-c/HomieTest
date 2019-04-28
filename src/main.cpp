9108c9e0f6add99770971230067959ec95724432
Constantin Bogza <tinel.c@gmail.com>
2019-04-28 18:48:22 +0300

    Add initial version of the software for IOT Homie for TinelIotProjects

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <Homie.h>
+
+HomieNode lightNode("light", "Light", "switch");
+
+bool globalInputHandler(const HomieNode& node, const HomieRange& range, const String& property, const String& value) {
+  Homie.getLogger() << "Received on node " << node.getId() << ": " << property << " = " << value << endl;
+  return true;
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial << endl << endl;
+  Homie_setFirmware("TinelIotBaseSoftware", "1.0.1");
+  Homie.setGlobalInputHandler(globalInputHandler);
+
+  lightNode.advertise("on").setName("On").setDatatype("boolean").settable();
+
+  // set the name of the device family
+  Homie_setBrand("T19SMWV01");
+  // stup homie
+  Homie.setup();
+}
+
+void loop() {
+  Homie.loop();
+}
